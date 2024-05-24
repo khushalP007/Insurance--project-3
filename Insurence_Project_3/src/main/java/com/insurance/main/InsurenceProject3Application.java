8a083d1ca9014ef3a0b071aa5d96d9a608d0d0c8
khushalP007 <chaudharikhushal334@gmail.com>
2024-05-24 22:05:59 +0530

    First Commit

---
+++ Insurence_Project_3/src/main/java/com/insurance/main/InsurenceProject3Application.java
@@ -0,0 +1,13 @@
+package com.insurance.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurenceProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurenceProject3Application.class, args);
+	}
+
+}
---
+++ Insurence_Project_3/src/test/java/com/insurance/main/InsurenceProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurenceProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
